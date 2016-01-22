bccdff5db63a4ae319ee89060e85d20c00c57c8b
Maciej Muchajer <mmuchajer@o2.pl>
2016-01-22 15:53:09 +0100

    Wykonany schemat strony

--- src/CarRentBundle/Controller/DefaultController.php
+++ src/CarRentBundle/Controller/DefaultController.php
@@ -15,4 +15,54 @@ class DefaultController extends Controller
     {
         return $this->render('CarRentBundle:Default:car.html.twig');
     }
+	
+	public function osoboweAction()
+    {
+        return $this->render('CarRentBundle:Default:osobowe.html.twig');
+    }
+	
+	public function dostawczeAction()
+    {
+        return $this->render('CarRentBundle:Default:dostawcze.html.twig');
+    }
+	
+	public function popularneAction()
+    {
+        return $this->render('CarRentBundle:Default:popularne.html.twig');
+    }
+	
+	public function ocenianeAction()
+    {
+        return $this->render('CarRentBundle:Default:oceniane.html.twig');
+    }
+	
+	public function warunkiAction()
+    {
+        return $this->render('CarRentBundle:Default:warunki.html.twig');
+    }
+	
+	public function os1Action()
+    {
+        return $this->render('CarRentBundle:Default:1.html.twig');
+    }
+	
+	public function os2Action()
+    {
+        return $this->render('CarRentBundle:Default:2.html.twig');
+    }
+	
+	public function os3Action()
+    {
+        return $this->render('CarRentBundle:Default:3.html.twig');
+    }
+	
+	public function do1Action()
+    {
+        return $this->render('CarRentBundle:Default:4.html.twig');
+    }
+	
+	public function do2Action()
+    {
+        return $this->render('CarRentBundle:Default:5.html.twig');
+    }
 }
