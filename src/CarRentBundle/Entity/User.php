de29629c7f46b28d9e713ac8c88fd531e1823d3e
Paweł Kalinowski <pawlo1337@gmail.com>
2016-02-03 12:57:41 +0100

    Dodanie panelu uzytkownika

--- src/CarRentBundle/Entity/User.php
+++ src/CarRentBundle/Entity/User.php
@@ -27,20 +27,6 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
-	
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="firstname", type="string", length=50)
-     */
-    private $firstname;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="lastname", type="string", length=50)
-     */
-    private $lastname;
 
     /**
      * Get id
@@ -52,53 +38,7 @@ class User extends BaseUser
         return $this->id;
     }
 
-    /**
-     * Set firstname
-     *
-     * @param string $firstname
-     *
-     * @return User
-     */
-    public function setFirstname($firstname)
-    {
-        $this->firstname = $firstname;
-
-        return $this;
-    }
-
-    /**
-     * Get firstname
-     *
-     * @return string
-     */
-    public function getFirstname()
-    {
-        return $this->firstname;
-    }
-
-    /**
-     * Set lastname
-     *
-     * @param string $lastname
-     *
-     * @return User
-     */
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    /**
-     * Get lastname
-     *
-     * @return string
-     */
-    public function getLastname()
-    {
-        return $this->lastname;
-    }
+ 
 
 }
 
